fd872472394787bc7257c9dfb7a85056d2b3f871
Roman <roma.klimov10.94@gmail.com>
2023-05-05 23:09:02 +1100

    Сохраняюсь , иду спать

---
+++ tesk024_sum/Program.cs
@@ -0,0 +1,35 @@
+﻿// Создать прогрмму которая будет принимать на вход число (n) и выводить сумму чисел от 1 до n
+// 7→ 28
+// 4→ 10
+// 8→36
+
+
+Console.WriteLine("введите число ...");
+int number = Convert.ToInt32(Console.ReadLine());
+int SumFrom1ToN(int n )
+{
+    int sum = 0;
+    for(int i = 0; i <= n; i++ )
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+Console.Clear();
+Console.Write($"Сумма всех числе от 1 до {number} = ");
+Console.WriteLine(SumFrom1ToN(number));
+
+// Console.WriteLine(SumFrom1ToN(1000)); можно и так ввести число, фукция же и так принимает Int значит с расопзнованием числа проблем не будет;)
+// Как еще можно представить эту функцию? Во первых есть определение.. sum всех чисел от 0 до n = n * (n + 1) / 2 давай спишем ее себе;)
+
+int FastSumFrom1ToN( int n)
+{
+    
+    return n * (n + 1)/2;              // так тоже все работет .. А вот например  так ↓     
+                                      // n = n * (n + 1)/2;
+                                     // return   ни хрена.. и даже int n = n * (n +1)/2 тоже нихрена..
+                                     // кароче можно ворачивать саму формулу если таковая есть
+                                      
+}
+
+Console.WriteLine(FastSumFrom1ToN(7));
---
+++ tesk026_NumberOfDigits/Program.cs
@@ -0,0 +1,18 @@
+﻿/// Напишите программу которая принимает на входе число n и показывает сколько цифр в этом числе 
+// 579 → 3;
+// 7831597 → 7;
+// 97 → 2;
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberOfDigits(int n)
+{
+    int count = 0;
+    for(int i = number; i != 0; count++ )
+    {
+        i = i / 10;
+    }
+    return count;
+}
+Console.WriteLine(NumberOfDigits(number));
